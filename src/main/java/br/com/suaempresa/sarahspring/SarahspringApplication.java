48567f4752c877488da73999625bdfec0fdf1ab8
Sarah Nicacio de Amorim <nicaciosrah085@gmail.com>
2024-05-28 22:32:23 -0300

    Create a Spring project

---
+++ src/main/java/br/com/suaempresa/sarahspring/SarahspringApplication.java
@@ -0,0 +1,23 @@
+package br.com.suaempresa.sarahspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SarahspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarahspringApplication.class, args);
+	}
+	@GetMapping("/hello")
+    public String hello() {
+ 
+		return "funciona pelo amor de Deus";
+    }
+
+}
